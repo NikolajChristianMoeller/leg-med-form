7d2094038a83232e9fd00c7fc0da514737efaec2
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-04-20 11:16:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ window.addEventListener("load", initApp);
 
 function start() {
     console.log("Javascript Kører");
-    document.querySelector("form#signup").addEventListener("click", clickSubmit);
+    document.querySelector("#signup").addEventListener("submit", clickSubmit);
 }
 
-function clickSubmit() {
-  console.log("Click submit)");
+function clickSubmit(event) {
+
+  event.preventDefault();
+  
+  console.log("Click submit");
+  
+  console.log(event.fullName);
+  
+  const elements = document.querySelector("form#signup").elements;
+
+  const signup = {
+    fullname: elements.namedItem("fullname").value,
+    payment: elements.namedItem("payment").value
+  }
+
+  console.log(signup);
 }
