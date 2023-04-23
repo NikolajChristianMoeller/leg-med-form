1a7a421ca523bdc9a77075e2ceff8ae4dd236a21
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-04-23 09:53:28 +0200

    Ændtinger
    Ændringer

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 "use strict";
 
-window.addEventListener("load", initApp);
+window.addEventListener("load", start);
 
 
 function start() {
     console.log("Javascript Kører");
-    document.querySelector("#signup").addEventListener("submit", clickSubmit);
+  document.querySelector("#signup").addEventListener("submit", clickSubmit);
+  document.querySelector("#signup-accept").addEventListener("click", clickAccept);
+}
+
+function clickAccept() {
+  console.log(event.target.checked);
+  // if (event.target.checked === true) {
+  //   document.querySelector("#btn-submit").disabled = false;
+  // } else {
+  //   document.querySelector("#btn-submit").disabled = true;
+  // }
+  document.querySelector("#btn-submit").disabled = !event.target.checked;
 }
 
 function clickSubmit(event) {
 
   event.preventDefault();
-  
+
   console.log("Click submit");
   
   console.log(event.fullName);
@@ -22,6 +33,21 @@ function clickSubmit(event) {
     fullname: elements.namedItem("fullname").value,
     payment: elements.namedItem("payment").value
   }
+
+  // // // ellers brug
+
+  // // const signup = {
+  // //   fullname: "",
+  // //   email: "",
+  // //   username: "",
+  // //   password: "",
+  // //   payevery: "",
+  // //   spam: true
+  // // }
+
+  // for (const prop in signup) {
+    
+  // })
 
   console.log(signup);
 }
